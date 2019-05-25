9967d85b3828718cbdbdd3b437c066af9028c6ca
Michael Bukachi <michaelbukachi@gmail.com>
2019-05-25 22:46:36 +0300

    build: Remove web folder as submodule

---
+++ web/vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  assetsDir: '../static',
+  baseUrl: '',
+  publicPath: undefined,
+  outputDir: path.resolve(__dirname, '../app/templates'),
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+};
