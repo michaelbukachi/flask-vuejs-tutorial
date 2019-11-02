1f645e5fc4f8afafd8c90ac61b4b955d4530c485
Michael Bukachi <michaelbukachi@gmail.com>
2019-11-02 16:24:31 +0300

    build: Improve waiting logic for http server

--- tests/test_vue_integration.py
+++ tests/test_vue_integration.py
@@ -1,9 +1,3 @@
-import socket
-from contextlib import closing
-
-
 def test_text_is_show(driver):
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        if sock.connect_ex(('localhost', 5000)) == 0:
-            driver.get('http://localhost:5000/views/sample')
-            assert 'Welcome to Your Vue.js App' in driver.page_source
+    driver.get('http://localhost:5000/views/sample')
+    assert 'Welcome to Your Vue.js App' in driver.page_source
